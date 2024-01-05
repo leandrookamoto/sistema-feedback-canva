da278f3a012d4d6919d5ca21e716eb97fbcfda60
leandrookamoto <leandro.okamoto@globalhitss.com.br>
2024-01-05 16:10:52 -0300

    adição do chart não dinamico

--- resources/js/src/Components/Chart.js
+++ resources/js/src/Components/Chart.js
@@ -1,20 +1,30 @@
-import {Doughnut} from 'react-chartjs-2';
+import {Line} from 'react-chartjs-2';
 import {Chart as ChartJS} from 'chart.js/auto';
 
-// const data = {
-//     labels: ['Dogs','Cats', 'Birds', 'Fish', 'Hamster'],
-//     datasets:[
-//         {label:'Popularity',
-//         data:[50,15,20,15,10]
-//         }
-//     ]
+const data = {
+    labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+    datasets: [
+        {
+            label: 'Feedbacks realizados',
+            data: [15, 20, 12, 17, 22, 25],
+            borderColor: 'blue',
+            backgroundColor:'blue'
 
-// }
+        },
+        {
+            label: 'Metas',
+            data: [22,22,22,22,22,22],
+            borderColor: 'red',
+            backgroundColor:'red'
+        }
+    ]
+};
 
-export default function Chart({data}){
+
+export default function Chart(){
 
 
-    return (<>
-        <Doughnut data={data}/>
-    </>)
+    return (<section style={{height: '500px', width: '100%'}}>
+        <Line data={data}/>
+    </section>)
 }
--- resources/js/src/Components/Home.js
+++ resources/js/src/Components/Home.js
@@ -1,43 +1,14 @@
+import Chart from './Chart';
 import './Home.css';
 
-export default function Home() {
+export default function Home({usuario}) {
   return (
     <>
-      <div class="ag-format-container">
-        <div class="ag-courses_box">
-          <div class="ag-courses_item">
-            <a href="#" class="ag-courses-item_link">
-              <div class="ag-courses-item_bg"></div>
+      <section>
+        <h3>Olá, {usuario}!</h3>
+        <Chart />
 
-              <div class="ag-courses-item_title">
-                UI/Web&amp;Graph design for teenagers 11-17&#160;years old
-              </div>
-            </a>
-          </div>
-
-          <div class="ag-courses_item">
-            <a href="#" class="ag-courses-item_link">
-              <div class="ag-courses-item_bg"></div>
-
-              <div class="ag-courses-item_title">
-                UX/UI Web-Design&#160;+ Mobile Design
-              </div>
-            </a>
-          </div>
-
-          <div class="ag-courses_item">
-            <a href="#" class="ag-courses-item_link">
-              <div class="ag-courses-item_bg"></div>
-
-              <div class="ag-courses-item_title">
-                Annual package "Product+UX/UI+Graph designer&#160;2022"
-              </div>
-            </a>
-          </div>
-
-
-        </div>
-      </div>
+      </section>
     </>
   );
 }
