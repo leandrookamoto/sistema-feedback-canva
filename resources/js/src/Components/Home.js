f0d3ea2f2d745370c80302f4e31322a3e828a4cd
leandrookamoto <leandro.okamoto@globalhitss.com.br>
2024-01-05 14:48:09 -0300

    Começo do home

--- resources/js/src/Components/Home.js
+++ resources/js/src/Components/Home.js
@@ -1,7 +1,43 @@
-export default function Home(){
-    return(
-        <>
-            Teste
-        </>
-    )
+import './Home.css';
+
+export default function Home() {
+  return (
+    <>
+      <div class="ag-format-container">
+        <div class="ag-courses_box">
+          <div class="ag-courses_item">
+            <a href="#" class="ag-courses-item_link">
+              <div class="ag-courses-item_bg"></div>
+
+              <div class="ag-courses-item_title">
+                UI/Web&amp;Graph design for teenagers 11-17&#160;years old
+              </div>
+            </a>
+          </div>
+
+          <div class="ag-courses_item">
+            <a href="#" class="ag-courses-item_link">
+              <div class="ag-courses-item_bg"></div>
+
+              <div class="ag-courses-item_title">
+                UX/UI Web-Design&#160;+ Mobile Design
+              </div>
+            </a>
+          </div>
+
+          <div class="ag-courses_item">
+            <a href="#" class="ag-courses-item_link">
+              <div class="ag-courses-item_bg"></div>
+
+              <div class="ag-courses-item_title">
+                Annual package "Product+UX/UI+Graph designer&#160;2022"
+              </div>
+            </a>
+          </div>
+
+
+        </div>
+      </div>
+    </>
+  );
 }
