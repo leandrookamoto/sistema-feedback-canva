b206cec4a6095770e46293573e7ed4e7e7343d83
leandrookamoto <leandro.okamoto@globalhitss.com.br>
2024-01-05 17:39:37 -0300

    novo

--- resources/js/src/Components/Home.js
+++ resources/js/src/Components/Home.js
@@ -3,6 +3,7 @@ import './Home.css';
 import { useState, useEffect } from 'react';
 
 export default function Home({ usuario, listaCadastro }) {
+  const [avaliacoesRealizadas, setAvaliacoesRealizadas] = useState([]);
   useEffect(() => {
     const lista = listaCadastro.map((item) => item.avaliacoes);
     let novaLista = lista.filter((item) => Array.isArray(JSON.parse(item)));
@@ -16,11 +17,12 @@ export default function Home({ usuario, listaCadastro }) {
 
       // Adiciona cada item (array) como um objeto ao arrayDeObjetos
       arrayDeObjetos.push(JSON.parse(item));
+
+      setAvaliacoesRealizadas(arrayDeObjetos);
     }
 
     // const novaLista2 = arrayDeObjetos.map(item=>JSON.parse(item.dados));
 
-  
     console.table(arrayDeObjetos);
   }, [listaCadastro]);
 
@@ -30,7 +32,7 @@ export default function Home({ usuario, listaCadastro }) {
     datasets: [
       {
         label: 'Feedbacks realizados',
-        data: [15, 20, 12, 17, 22, 25],
+        data: Array(50).fill(avaliacoesRealizadas.length),
         borderColor: 'blue',
         backgroundColor: 'blue',
       },
@@ -38,7 +40,7 @@ export default function Home({ usuario, listaCadastro }) {
         label: 'Metas',
         /*Aqui a meta é automaticamente preenchido de acordo com o número dos
         feedbacks de cima e de acordo com o número de funcionários cadastrados*/
-        data: Array(listaCadastro.length).fill(listaCadastro.length),
+        data: Array(50).fill(listaCadastro.length),
         borderColor: 'red',
         backgroundColor: 'red',
       },
