ab0a9ce837554043f761dea8668a45e6709b54da
leandrookamoto <leandro.okamoto@globalhitss.com.br>
2024-01-22 10:51:51 -0300

    implementação do formulário do plano iniciado

--- resources/js/src/Components/Planodeacao.js
+++ resources/js/src/Components/Planodeacao.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import Card from './Card';
 import Pagination from './Pagination';
+import Lista from './Lista';
+import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
 
-export default function Planodeacao({listaCadastro, avalDoFuncionario}) {
+export default function Planodeacao({ listaCadastro, avalDoFuncionario }) {
   //Constantes para controle de data
   const [mes, setMes] = useState('');
   const anoAtual = new Date().getFullYear();
@@ -21,10 +23,16 @@ export default function Planodeacao({listaCadastro, avalDoFuncionario}) {
     'Novembro',
     'Dezembro',
   ];
-  console.log('listaCadastro',listaCadastro)
-  console.log('avalDoFuncionario',avalDoFuncionario)
-  console.log('mes',mes)
-  console.log('ano',ano)
+  console.log('listaCadastro', listaCadastro);
+  console.log('avalDoFuncionario', avalDoFuncionario);
+  console.log('mes', mes);
+  console.log('ano', ano);
+
+  //Constantes para gravação de estado
+  const [inputs, setInputs] = useState([{ value: '', feito: false }]);
+
+  //Constantes para renderização do plano de ação
+  const [plano, setPlano] = useState(false);
 
   //Constantes que controlam o page
   const [page, setPage] = useState(1);
@@ -46,10 +54,10 @@ export default function Planodeacao({listaCadastro, avalDoFuncionario}) {
     return true;
   });
 
-  console.log('listaCadastrados',listaCadastrados)
+  console.log('listaCadastrados', listaCadastrados);
 
-   //Aqui somente faz o parse das avaliações para facilitar, pois vem como stringfy do banco
-   const comparaCadastrados = listaCadastrados.map((objeto) => {
+  //Aqui somente faz o parse das avaliações para facilitar, pois vem como stringfy do banco
+  const comparaCadastrados = listaCadastrados.map((objeto) => {
     if (objeto.avaliacoes && typeof objeto.avaliacoes === 'string') {
       try {
         return { ...objeto, avaliacoes: JSON.parse(objeto.avaliacoes) };
@@ -58,11 +66,10 @@ export default function Planodeacao({listaCadastro, avalDoFuncionario}) {
       }
     }
     return objeto;
-
   });
-  
-  console.log('comparaCadastrados',comparaCadastrados);
-    //Aqui pega os valores do banco de dados do app dos funcionários e faz o parse na chave avaliacoes
+
+  console.log('comparaCadastrados', comparaCadastrados);
+  //Aqui pega os valores do banco de dados do app dos funcionários e faz o parse na chave avaliacoes
   //para facilitar
   let comparaAvaliacoesFuncionario = avalDoFuncionario.map((objeto) => {
     // Verifica se o objeto tem a chave 'avaliacoes' e se o valor é uma string JSON
@@ -79,7 +86,7 @@ export default function Planodeacao({listaCadastro, avalDoFuncionario}) {
     return objeto;
   });
 
-  console.log('comparaAvaliacoesFuncionario',comparaAvaliacoesFuncionario)
+  console.log('comparaAvaliacoesFuncionario', comparaAvaliacoesFuncionario);
 
   //Aqui pega as avaliações realizadas pelos funcionarios e retorna somente aquelas que forem iguais
   //as datas selecionadas pelo usuário
@@ -102,7 +109,7 @@ export default function Planodeacao({listaCadastro, avalDoFuncionario}) {
     },
   );
 
-  console.log('verificaDataFuncionario',verificaDataFuncionario)
+  console.log('verificaDataFuncionario', verificaDataFuncionario);
 
   const listaFeedChefe3 = comparaCadastrados.filter((objetoA) => {
     try {
@@ -121,7 +128,7 @@ export default function Planodeacao({listaCadastro, avalDoFuncionario}) {
     }
   });
 
-  console.log('listaFeedChefe3',listaFeedChefe3)
+  console.log('listaFeedChefe3', listaFeedChefe3);
 
   //Aqui faz a comparação se existe o valor do verificaDataFuncionario e do listaChefe2, pois se existir
   //significa que ambos fizeram o feedback e retorna numa lista final
@@ -142,6 +149,31 @@ export default function Planodeacao({listaCadastro, avalDoFuncionario}) {
   function handleChange(event, value) {
     setPage(Math.min(value, totalPage));
   }
+
+  //Funções principais
+  function gravar() {
+    const lista = inputs.map((input) => input.value);
+    console.log(lista);
+    setPlano(true);
+  }
+
+  //Funções para deixar os inputs dinâmicos
+  const handleInputChange = (event, index) => {
+    const newInputs = [...inputs];
+    const novaLista = event.currentTarget.value;
+
+    console.log('novaLista', novaLista);
+
+    // Certifique-se de que o índice existe no array antes de acessar a propriedade
+    if (newInputs[index]) {
+      newInputs[index].value = novaLista;
+      setInputs(newInputs);
+    }
+  };
+
+  const handleAddInput = () => {
+    setInputs([...inputs, { value: '' }]);
+  };
 
   return (
     <>
@@ -222,26 +254,65 @@ export default function Planodeacao({listaCadastro, avalDoFuncionario}) {
         </select>
       </div>
       {listaFinal2.map((item) => {
-                  return (
-                    <div
-                      key={item.id}
-                      onClick={() => selecionarFuncionario(item.id)}
-                    >
-                      <Card
-                        nome={item.nome}
-                        email={item.email}
-                        setor={item.setor}
-                        chefe={item.administrador}
-                        botao1="Selecionar"
-                      />
-                    </div>
-                  );
-                })}
-                <Pagination
-                  page={page}
-                  handleChange={handleChange}
-                  totalPage={totalPage}
+        return (
+          <div key={item.id} onClick={() => selecionarFuncionario(item.id)}>
+            <Card
+              nome={item.nome}
+              email={item.email}
+              setor={item.setor}
+              chefe={item.administrador}
+              botao1="Selecionar"
+            />
+          </div>
+        );
+      })}
+      <Pagination
+        page={page}
+        handleChange={handleChange}
+        totalPage={totalPage}
+      />
+
+      {/* Parte da renderização do plano de ação */}
+      {listaFinal2.length > 0 && !plano && (
+        <>
+          <h5>Plano de ação</h5>
+
+          <div className="mb-1 mt-6">
+            <label htmlFor="exampleFormControlInput1" className="form-label">
+              Plano
+            </label>
+            {inputs.map((input, index) => (
+              <div key={index}>
+                <input
+                  type="text"
+                  value={input.value}
+                  onChange={event => handleInputChange(event, index)
+                  }
+                  placeholder="Colocar o plano de ação. Ex: Fazer treinamentos"
+                  className="form-control mb-2"
                 />
+              </div>
+            ))}
+            <AddCircleRoundedIcon
+              style={{ color: '#0d6efd', fontSize: '40px', cursor: 'pointer' }}
+              onClick={handleAddInput}
+            />
+          </div>
+          <button
+            type="button"
+            className="btn btn-primary mt-1"
+            onClick={gravar}
+          >
+            Gravar
+          </button>
+        </>
+      )}
+      {listaFinal2.length > 0 && plano && (
+        <>
+          <h5 className="mt-3">Plano de ação</h5>
+          <Lista />
+        </>
+      )}
     </>
   );
 }
