9d8a8b3ae1e5483650c964bb70ccd8ca9581dcdb
leandrookamoto <leandro.okamoto@globalhitss.com.br>
2024-01-26 15:11:45 -0300

    Metas ok

--- resources/js/src/Components/Home.js
+++ resources/js/src/Components/Home.js
@@ -11,7 +11,7 @@ export default function Home({ usuario, listaCadastro }) {
   
 
   // Obter o mês atual (retornado como um número, onde janeiro é 0 e dezembro é 11)
-const mesAtual = 6;
+  const mesAtual = dataAtual.getMonth();
 
 // Lista de nomes dos meses
 const meses = [
@@ -25,6 +25,9 @@ for (let i = mesAtual; i > mesAtual - 5; i--) {
     ultimosMeses.unshift(meses[i]);
   }
 }
+
+//Constante para verificar o número da meta
+const meta = (listaCadastro.length*0.86).toFixed(0);
 
 // Exibir o array resultante
 console.log(ultimosMeses);
@@ -107,7 +110,10 @@ console.log(ultimosMeses);
               <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12 12 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A20 20 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a20 20 0 0 0 1.349-.476l.019-.007.004-.002h.001M14 1.221c-.22.078-.48.167-.766.255-.81.252-1.872.523-2.734.523-.886 0-1.592-.286-2.203-.534l-.008-.003C7.662 1.21 7.139 1 6.5 1c-.669 0-1.606.229-2.415.478A21 21 0 0 0 3 1.845v6.433c.22-.078.48-.167.766-.255C4.576 7.77 5.638 7.5 6.5 7.5c.847 0 1.548.28 2.158.525l.028.01C9.32 8.29 9.86 8.5 10.5 8.5c.668 0 1.606-.229 2.415-.478A21 21 0 0 0 14 7.655V1.222z" />
             </svg>
           </div>
-          <div style={{ width: '60%', padding: '5px' }}>Metas</div>
+          <div style={{ width: '60%', padding: '5px' }}>
+            <div>Meta</div>
+            <div>{meta}</div>
+          </div>
         </div>
         <div
           style={{
@@ -171,7 +177,7 @@ console.log(ultimosMeses);
               <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
             </svg>
           </div>
-          <div style={{ width: '60%', padding: '5px' }}>Feedbacks que faltam</div>
+          <div style={{ width: '60%', padding: '5px' }}>Faltam</div>
         </div>
         <div
           style={{
@@ -201,7 +207,7 @@ console.log(ultimosMeses);
               <path d="M7 0a2 2 0 0 0-2 2H1.5A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14H2a.5.5 0 0 0 1 0h10a.5.5 0 0 0 1 0h.5a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2H11a2 2 0 0 0-2-2zM6 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1zM3 13V3h1v10zm9 0V3h1v10z" />
             </svg>
           </div>
-          <div style={{ width: '60%', padding: '5px' }}>Funcionários de férias</div>
+          <div style={{ width: '60%', padding: '5px' }}>Férias</div>
         </div>
       </div>
 
